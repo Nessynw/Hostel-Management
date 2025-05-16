88ca3b02333fefb219c45ea504bd253c584cfdb6
Ines <ines.zitouni03@gmail.com>
2025-05-16 16:18:42 +0200

    EtatChambres+ControlerReservation+PersonnelButtonControler

---
+++ src/main/java/Controler/PersonnelButtonControleur.java
@@ -0,0 +1,30 @@
+package Controler;
+
+import Vue.Authentification;
+import Model.Hotel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Le nom de la classe correspond maintenant au nom du fichier
+public class PersonnelButtonControleur implements ActionListener {
+    private JFrame parentFrame;
+    private Hotel hotel;
+
+    public PersonnelButtonControleur(JFrame parentFrame, Hotel hotel) {
+        this.parentFrame = parentFrame;
+        this.hotel = hotel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Authentification authentification = new Authentification(() -> {
+            PersonnelController controller = new PersonnelController(parentFrame, hotel);
+            parentFrame.getContentPane().removeAll();
+            parentFrame.getContentPane().add(controller.getVue());
+            parentFrame.revalidate();
+            parentFrame.repaint();
+        }, hotel);
+        authentification.setVisible(true);
+    }
+}
